55d162ab93dbecdb0776ce12580d4c5c33a6fdb8
Pasha <vishnyakpavelh.gmail.com>
2026-05-14 19:41:35 +0200

    Подготовка к деплою на Render

--- backend/main.py
+++ backend/main.py
@@ -407,4 +407,4 @@ def serve_react(path):
     return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
